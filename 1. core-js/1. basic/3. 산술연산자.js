68d065b979d8b7820c310c901738ad36b9e56c2a
Reppy <dlgy9714@gmail.com>
2023-07-24 15:27:55 +0900

    자바스크립트 기본

---
+++ 1. core-js/1. basic/3. 산술연산자.js
@@ -0,0 +1,32 @@
+// 거듭제곱 연산
+let num=3;
+console.log(num**4);
+
+// 증감 연산자
+var x=5;
+x++;
+console.log(`x:${x}`);
+
+let y = 33;
+console.log(--y);
+
+let a=7;
+let b=a++;
+console.log(`a: ${a},b: ${b}`);
+
+let c=7;
+let d=++c;
+
+console.log(`a: ${c},b: ${d}`);
+
+// 대입 연산자
+let z=9;
+z*=10;
+z/=8;//5
+z**2;//25
+
+
+let m =8;
+m++;
+console.log(`m: ${m}`);
+console.log(`z: ${z}`);
---
+++ app.js
@@ -0,0 +1,13 @@
+// alert("흠냐")
+
+// const a=10, b=20;
+// console.log(a*b);
+
+const x=10;
+const u=2;
+console.log(x+u)
+
+function foo(){
+    return{age: 10, nick: '흠냐'};
+}
+console.log(foo());
