0d3bd13a9d9f7ea043b802eb7e20663b0cddbf74
Reppy <dlgy9714@gmail.com>
2023-07-28 10:44:35 +0900

    chapter03끝

---
+++ 1. core-js/6. 함수/10. let과 const.js
@@ -0,0 +1,24 @@
+let x;
+console.log(x);
+const y='';
+console.log(y);
+
+// 
+const COLOR_YELLOW = '#ff0;'
+const COLOR_GREEN = '#0f0;'
+const COLOR_BLACK = '#000;'
+
+// $div.style.background = COLOR_YELLOW;
+// $div.style.background = COLOR_GREEN;
+// $div.style.background = COLOR_BLACK;
+
+const kim={
+    name:'철수',
+    age:30
+};
+// kim={
+//     name:'영희',
+//     age:40
+// };
+kim.name='이게 되네?'
+console.log(kim);
